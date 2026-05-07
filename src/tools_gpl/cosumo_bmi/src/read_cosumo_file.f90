ddcd27380470c535733fc4ff71c637311e433f39
Robert King <robcking@stanford.edu>
2026-05-06 20:47:23 -0700

    pointer types

--- src/tools_gpl/cosumo_bmi/src/read_cosumo_file.f90
+++ src/tools_gpl/cosumo_bmi/src/read_cosumo_file.f90
@@ -260,12 +260,15 @@ subroutine find_n()
     !
     ! Locals
     integer :: idis
+    ! --- THE CORRECT TYPE AND RANK FOR THE MASK POINTER ---
+    integer, pointer :: null_mask(:) => null()
+    ! ------------------------------------------------------
     !
     ! Body
-    call nearest_neighbour(nf_num_dif, x_diff  , y_diff  , null(), n_diff  , Huge(1.d0), fm_xzw, fm_yzw, fm_ndx, 0, 0)
-    call nearest_neighbour(nf_num_dif, x_intake, y_intake, null(), n_intake, Huge(1.d0), fm_xzw, fm_yzw, fm_ndx, 0, 0)
+    call nearest_neighbour(nf_num_dif, x_diff  , y_diff  , null_mask, n_diff  , Huge(1.d0), fm_xzw, fm_yzw, fm_ndx, 0, 0)
+    call nearest_neighbour(nf_num_dif, x_intake, y_intake, null_mask, n_intake, Huge(1.d0), fm_xzw, fm_yzw, fm_ndx, 0, 0)
     do idis = 1, nf_num_dif
-        call nearest_neighbour(no_amb(idis), x_amb(idis,:), y_amb(idis,:), null(), n_amb(idis,:), Huge(1.d0), fm_xzw, fm_yzw, fm_ndx, 0, 0)
+        call nearest_neighbour(no_amb(idis), x_amb(idis,:), y_amb(idis,:), null_mask, n_amb(idis,:), Huge(1.d0), fm_xzw, fm_yzw, fm_ndx, 0, 0)
     enddo
 end subroutine find_n
 
